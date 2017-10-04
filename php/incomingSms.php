9fbccfe3a0fc57efda62d58dc327f610a8810995
franky-naidu <franky16081996@gmail.com>
2017-10-04 10:21:09 +0530

    Extract the GEt parameters from urk

--- php/incomingSms.php
+++ php/incomingSms.php
@@ -9,4 +9,13 @@
 	if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
 		exit();
 	}	
+
+	
+	//http://localhost:8080/incomingSms.php?SmsSid=132456&From=9052161119&To=56070&Date=12/2/2017&Body=Hello+World
+	//Extratct the GEt parameters from url 
+	$SmsSid = $_GET["SmsSid"];
+	$From = $_GET["From"];
+	$To = $_GET["To"];
+	$Date = $_GET["Date"];
+	$Body = $_GET["Body"];
 ?>
