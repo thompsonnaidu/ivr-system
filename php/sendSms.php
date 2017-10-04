3729d34a8ef81eeca6a10d747234d63b79b63457
franky-naidu <franky16081996@gmail.com>
2017-10-04 10:34:25 +0530

    Send sms boiler plate

---
+++ php/sendSms.php
@@ -0,0 +1,21 @@
+<?php
+
+	//Call sendSms when you want to send a message
+	// $From :- The sender mobile number.It is automatically replaced by the exotel mobile number
+	// $To :- The mobile number of the receiver.
+	// $msg :- The sms the sender wants to send.
+	function sendSms($From,$To,$msg)
+	{
+		// To send an sms we make a post request to url specified by exotel
+
+		//Set the Post data
+		$post_data = array(	    
+		    'From'   => $From,
+		    'To'    => $To,
+		    'Body'  => $msg,
+		);
+	}
+	
+
+
+?>
