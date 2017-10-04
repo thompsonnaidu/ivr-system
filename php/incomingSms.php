a45d56b490c3f300d506c48b6e21d235b003e823
franky-naidu <franky16081996@gmail.com>
2017-10-04 10:18:32 +0530

    Handling HEAD request in data

--- php/incomingSms.php
+++ php/incomingSms.php
@@ -3,4 +3,10 @@
 	$username = "root";
 	$password = "";
 	$dbname = "exotel";
+	//content type must be set to text/plain
+	header('Content-Type: text/plain');
+	//exotel sends a HEAD request to verify the headers
+	if ($_SERVER['REQUEST_METHOD'] == 'HEAD') {
+		exit();
+	}	
 ?>
